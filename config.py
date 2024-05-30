efe802214a891e262f9018e9f04ec35d6aec32b7
BotzLove <157481428+BotzLove@users.noreply.github.com>
2024-05-30 15:54:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "https://av-file-storb-was-d.onrender.com/"
     else:
-        URL = ""
+        URL = "https://av-file-storb-was-d.onrender.com/"
 
 
 
