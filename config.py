57411fb55bad3bccf27cb23dd281461e25620ba4
xxxx2626 <avbotz26@gmail.com>
2024-11-01 08:26:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -105,9 +105,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://rear-estrella-avbot-506c5f13.koyeb.app/"
+        URL = "https://useful-ainslie-avbotz-5d04c5bb.koyeb.app/"
     else:
-        URL = "https://rear-estrella-avbot-506c5f13.koyeb.app/"
+        URL = "https://useful-ainslie-avbotz-5d04c5bb.koyeb.app/"
 
 
 
